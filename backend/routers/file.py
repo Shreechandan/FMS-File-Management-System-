d91a0e1992ef4f5f6ebfbb6afc7880e27dee7b0b
Shreechandan Behera <146723408+Shreechandan@users.noreply.github.com>
2024-07-12 08:20:28 +0530

    Add files via upload

---
+++ backend/routers/file.py
@@ -0,0 +1,55 @@
+import shutil
+from fastapi import APIRouter, Depends, UploadFile, File as FastAPIFile, HTTPException
+from app.schemas import ResponseModel, User
+from app.crud import add_file, delete_file, get_full_folder_path, retrieve_files_by_folder_id
+from app.dependencies import get_current_admin
+
+router = APIRouter(
+    prefix="/files",
+    tags=["files"],
+)
+
+
+@router.post("/", response_description="File data added into the database")
+async def upload_file(folder_id: str, file: UploadFile = FastAPIFile(...), current_admin: User = Depends(get_current_admin)):
+    if not current_admin:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    folder_path = await get_full_folder_path(folder_id)
+    file_path = f"{folder_path}/{file.filename}" if folder_path else file.filename
+
+    file_data = {
+        "Name": file.filename,
+        "FolderID": folder_id,
+        "Path": file_path,  # Store the full file path
+        "URL": f"/uploads/{file.filename}",
+        "OwnerID": dict(current_admin)["username"]
+    }
+
+    new_file = await add_file(file_data)
+
+    # Save the file to the server
+    with open(f'app/uploads/{file.filename}', 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return ResponseModel(data=new_file, message="File uploaded successfully")
+
+
+@router.get("/{folder_id}/files", response_description="Files retrieved")
+async def get_files_in_folder(folder_id: str):
+    files = await retrieve_files_by_folder_id(folder_id)
+    return ResponseModel(data=files, message="Files retrieved successfully")
+
+
+@router.delete("/{file_id}", response_description="File deleted from the database")
+async def delete_file_by_id(file_id: str, current_admin: User = Depends(get_current_admin)):
+    if not current_admin:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    file, error_message = await delete_file(file_id)
+    if file:
+        return ResponseModel(file, f"File with id: {file_id} deleted successfully.")
+    if error_message:
+        raise HTTPException(
+            status_code=500, detail=f"Error deleting file: {error_message}")
+    return ResponseModel(file, f"No file found with id {file_id}.")
---
+++ backend/routers/folder.py
@@ -0,0 +1,55 @@
+# app/routers/folder.py
+
+from fastapi import APIRouter, Depends, HTTPException
+from app.schemas import ResponseModel, User, folderSchema
+from app.crud import add_folder, delete_folder, retrieve_files_by_folder_id, retrieve_folders, retrieve_folders_by_parent_id, search_folders_and_files
+from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+from app.dependencies import get_current_admin
+
+router = APIRouter(
+    prefix="/folders",
+    tags=["folders"],
+)
+
+
+@router.post("/", response_description="Folder data added into the database")
+async def add_folder_data(Folder: folderSchema = Body(...), current_admin: User = Depends(get_current_admin)):
+    Folder = jsonable_encoder(Folder)
+    new_Folder = await add_folder(Folder)  # type: ignore
+    return ResponseModel(new_Folder, "Folder added successfully.")
+
+
+@router.get("/", response_description="Folder retrieved")
+async def get_folder():
+    folder = await retrieve_folders()
+    if folder:
+        return ResponseModel(folder, "folder data retrieved successfully")
+    return ResponseModel(folder, "Empty list returned")
+
+
+@router.get("/{parent_id}", response_description="Folders retrieved by parent folder ID")
+async def get_folders_by_parent_id(parent_id: str):
+    folders = await retrieve_folders_by_parent_id(parent_id)
+    files = await retrieve_files_by_folder_id(parent_id)
+    if folders or files:
+        return ResponseModel(folders + files, f"Folders with ParentfolderID {parent_id} retrieved successfully.")
+    return ResponseModel(folders, f"No folders found with ParentfolderID {parent_id}.")
+
+
+@router.delete("/{id}", response_description="Folder deleted by id")
+async def delete_folder_by_id(id: str, current_admin: User = Depends(get_current_admin)):
+    if not current_admin:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    folder = await delete_folder(id)
+    if folder:
+        return ResponseModel(folder, f"Folder with id: {id} deleted successfully.")
+    return ResponseModel(folder, f"No folder found with id {id}.")
+
+
+# Search ---------------------------
+@router.get("/search/", response_description="Search folders and files by name")
+async def search_folders_and_files_by_name(query: str):
+    result = await search_folders_and_files(query)
+    return ResponseModel(result, "Search results retrieved successfully")
